7073dc0a0859c519fbbbc3089e20e88cae5c032b
monicavaldovinos <20243ds062@utez.edu.mx>
2026-06-13 12:53:15 -0600

    Configuración inicial del proyecto con Spring Boot

---
+++ src/main/java/com/hvb/loyalty/HvbBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hvb.loyalty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HvbBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HvbBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hvb/loyalty/HvbBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hvb.loyalty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HvbBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
